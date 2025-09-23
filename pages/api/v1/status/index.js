23e0a27e3a94a00de5be62ea8239560b8d0ef631
Vinicius Vitório Rodrigues <62451033+viniciusVitorio@users.noreply.github.com>
2025-09-23 00:44:11 +0000

    Adjustments to the `/status` endpoint.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,23 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum');
 
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).end();
+  const version = await database.query('SELECT VERSION()');
+
+  const maxConnections = await database.query('SHOW max_connections');
+
+  const usedConnections = await database.query('SELECT count(*) as usedConnections FROM pg_stat_activity;');
+
+  response.status(200).json({
+    updated_at : updatedAt,
+    database : {
+      version : version.rows[0].version,
+      max_connections : maxConnections.rows[0].max_connections,
+      used_connections : usedConnections.rows[0].usedconnections,
+    }
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test('get to /api/v1/status should return 200', async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  
+  const responseBody = await response.json()
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  expect(responseBody.database.version).toBeDefined();
+
+  expect(responseBody.database.max_connections).toBeDefined();
+  
+  expect(responseBody.database.used_connections).toBeDefined();
 })
